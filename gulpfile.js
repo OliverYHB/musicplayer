92fb73f3e84c5b77ed6481c96dd6d89850878832
OliverYHB <15545570709@163.com>
2018-08-23 22:09:44 +0800

    2018.8.23初始gulp配置

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp");
+// 用插件开服务器
+var connect = require("gulp-connect");
+var less = require("gulp-less")
+
+gulp.task("task2",function(){
+    console.log(222);
+})
+
+gulp.task("task1",["task2"],function(){
+    console.log(111);
+})
+
+
+gulp.task("html",function(){
+    gulp.src("./src/index.html")
+    .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload())
+})
+
+gulp.task("watch",function(){
+    gulp.watch("./src/index.html",["html"])
+    gulp.watch("./src/less/*.less",["less"])
+})
+
+gulp.task("less",function(){
+    gulp.src("./src/less/*.less")
+    .pipe(less())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(connect.reload())
+
+})
+gulp.task("server",function () {
+    connect.server({
+        root: "dist",
+        port: "3939",
+        livereload:true
+    })
+})
+
+gulp.task("default",["html","watch","server","less"])
